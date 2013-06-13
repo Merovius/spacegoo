3eceb1ebfa3e0cc06f022d41eb91b2e730806ed5
Axel Wagner <mail@merovius.de>
2013-06-14 00:49:02 +0200

    Add a lot of documentation

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"sort"
 )
 
+// Ships designates a tuple of Ships e.g. for fleets, production…
 type Ships [3]int
+
 type fShips [3]float64
 type Fleets []Fleet
 
@@ -18,38 +20,45 @@ const (
 	They
 )
 
+// Bot is the primary interface to the API.
+//
+// You specify in every Round, given a GameState, what you want to do.
+// If you return an error, you loose - so better don't do that.
+//
+// The canonical Moves are GameState.Nop and GameState.Send
 type Bot interface {
 	Move(GameState) (Move, error)
 }
 
-/* The complete gamestate, we get each round */
+// GameState is the complete State, given each round, pretty much as specified
+// in the protocol
 type GameState struct {
-	Round     int
-	MaxRounds int
-	GameOver  bool
-	Fleets    Fleets
-	Planets   []Planet
+	Round     int      // The current Gameround
+	MaxRounds int      // The maximum number of rounds
+	GameOver  bool     // If the game is over
+	Fleets    Fleets   // All fleets on their way currently
+	Planets   []Planet // All Planets of all Players
 	pid       int
 }
 
-/* A fleet */
+// Fleet is a fleet currently on it's way
 type Fleet struct {
-	Id     int
-	Owner  Player
-	Origin Planet
-	Target Planet
-	Ships  Ships
-	Eta    int
+	Id     int    // The Id of this fleet. Use this for comparisons
+	Owner  Player // The Owner
+	Origin Planet // Where this fleet comes from
+	Target Planet // Where this fleet is going
+	Ships  Ships  // How many ships?
+	Eta    int    // How long until it arrives
 }
 
-/* A single planet as reported by the server */
+// Planet is a planet
 type Planet struct {
-	X          int
-	Y          int
-	Production Ships
-	Ships      Ships
-	Owner      Player
-	Id         int
+	Id         int    // The Id of this planet. Use this for comparisons
+	X          int    // The X-coordinate
+	Y          int    // The Y-coordinate
+	Production Ships  // How many ships this planet produces
+	Ships      Ships  // How many ships are stationed here
+	Owner      Player // Who owns this planet
 }
 
 func battleRound(mine, other fShips) fShips {
@@ -85,6 +94,7 @@ func battleRound(mine, other fShips) fShips {
 	return other
 }
 
+// Sum returns the absolute size of this fleet
 func (s Ships) Sum() int {
 	return s[0] + s[1] + s[2]
 }
@@ -100,6 +110,7 @@ func (f fShips) Sum() float64 {
 	return f[0] + f[1] + f[2]
 }
 
+// Add another fleet to this one
 func (s1 Ships) Add(s2 Ships) (s3 Ships) {
 	for i := 0; i < 2; i++ {
 		s3[i] = s1[i] + s2[i]
@@ -114,10 +125,12 @@ func (f fShips) Ships() (s Ships) {
 	return
 }
 
+// String formats the fleet easily readable
 func (s Ships) String() string {
 	return fmt.Sprintf("(%d %d %d)", s[0], s[1], s[2])
 }
 
+// Simulate a battle between two fleets
 func Simulate(mine, other Ships) (minenew, othernew Ships) {
 	mineS := mine.float()
 	otherS := other.float()
@@ -131,6 +144,7 @@ func Simulate(mine, other Ships) (minenew, othernew Ships) {
 	return mineS.Ships(), otherS.Ships()
 }
 
+// MyPlanets gives you all Planets under your control
 func (s *GameState) MyPlanets() (my []Planet) {
 	for _, p := range s.Planets {
 		if p.Owner == We {
@@ -140,6 +154,7 @@ func (s *GameState) MyPlanets() (my []Planet) {
 	return
 }
 
+// NotMyPlanets gives you all neutral and enemy Planets
 func (s *GameState) NotMyPlanets() (theirs []Planet) {
 	for _, p := range s.Planets {
 		if p.Owner != We {
@@ -149,6 +164,7 @@ func (s *GameState) NotMyPlanets() (theirs []Planet) {
 	return
 }
 
+// TheirPlanets gives you all enemy Planets
 func (s *GameState) TheirPlanets() (enem []Planet) {
 	for _, p := range s.Planets {
 		if p.Owner == They {
@@ -158,6 +174,7 @@ func (s *GameState) TheirPlanets() (enem []Planet) {
 	return
 }
 
+// NeutralPlanets gives you all neutral Planets
 func (s *GameState) NeutralPlanets() (neutr []Planet) {
 	for _, p := range s.Planets {
 		if p.Owner == Neutral {
@@ -167,7 +184,7 @@ func (s *GameState) NeutralPlanets() (neutr []Planet) {
 	return
 }
 
-// Calculate the distance of Point (x,y) from the Planet
+// Dist calculates the distance of the point (x,y) from this Planet
 func (p1 *Planet) Dist(x, y float64) float64 {
 	dx := float64(p1.X) - x
 	dy := float64(p1.Y) - y
@@ -192,6 +209,7 @@ func (fs fleetSorter) Swap(i, j int) {
 	fs.f[i] = t
 }
 
+// Sort the Fleets ascending by ETA
 func (f Fleets) Sort() {
 	sort.Sort(fleetSorter{f})
 }

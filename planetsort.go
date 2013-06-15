b3523c07bf6ec52eccee33d6ed7e8b07dd9ec0e3
Axel Wagner <mail@merovius.de>
2013-06-16 01:41:10 +0200

    Sort planets by fleetsize

--- planetsort.go
+++ planetsort.go
@@ -13,6 +13,8 @@ type planetsByDist struct {
 	FY      float64
 }
 
+type planetsByShips Planets
+
 func (sorter planetsByDist) Len() int {
 	return len(sorter.Planets)
 }
@@ -32,10 +34,25 @@ func (sorter planetsByDist) Swap(i, j int) {
 	sorter.Planets[i] = t
 }
 
+func (sorter planetsByShips) Len() int {
+	return len(sorter)
+}
+
+func (sorter planetsByShips) Less(i, j int) bool {
+	return sorter[i].Ships.Sum() < sorter[j].Ships.Sum()
+}
+
+func (sorter planetsByShips) Swap(i, j int) {
+	t := sorter[i]
+	sorter[i] = sorter[j]
+	sorter[j] = t
+}
+
 // SortByFDist sorts the planets by distance from a given point (in
 // float-coordinates)
 func (pl Planets) SortByFDist(X, Y float64) Planets {
 	plcpy := make(Planets, len(pl))
+	copy(plcpy, pl)
 	sorter := planetsByDist{plcpy, false, 0, 0, X, Y}
 	sort.Sort(sorter)
 	return sorter.Planets
@@ -45,7 +62,16 @@ func (pl Planets) SortByFDist(X, Y float64) Planets {
 // int-coordinates)
 func (pl Planets) SortByDist(X, Y int) Planets {
 	plcpy := make(Planets, len(pl))
+	copy(plcpy, pl)
 	sorter := planetsByDist{plcpy, false, X, Y, 0.0, 0.0}
 	sort.Sort(sorter)
 	return sorter.Planets
+}
+
+// SortByShips sorts the planets in ascending order by power of fleetsize.
+func (pl Planets) SortByShips() Planets {
+	plcpy := make(Planets, len(pl))
+	copy(plcpy, pl)
+	sort.Sort(planetsByShips(plcpy))
+	return pl
 }

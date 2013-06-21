ada779fccb6884c8b6661bcd1521fcee7b66c9bf
Axel Wagner <mail@merovius.de>
2013-06-21 02:45:18 +0200

    Mobber Bot
    Always trying to bash the weakest planet

---
+++ mobber/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	. "github.com/Merovius/spacegoo"
+	"github.com/Merovius/spacegoo/boilerplate"
+	"math/rand"
+)
+
+type Mobber struct {
+	init   bool
+	Victim Planet
+}
+
+func chooseVictim(state GameState) Planet {
+	NotMine := state.NotMyPlanets()
+	NotMine = NotMine.SortByShips()
+	if len(NotMine) == 0 {
+		n := rand.Intn(len(state.Planets))
+		return state.Planets[n]
+	}
+
+	return NotMine[0]
+}
+
+func (bot *Mobber) Move(state GameState) Move {
+	if !bot.init {
+		bot.Victim = chooseVictim(state)
+		bot.init = true
+	}
+	bot.Victim = state.Planets[bot.Victim.Id]
+	if bot.Victim.Owner == We || state.Round%10 == 0 {
+		bot.Victim = chooseVictim(state)
+	}
+
+	Mine := state.MyPlanets()
+	Mine = Mine.SortByShips()
+
+	if len(Mine) == 0 {
+		return Nop{}
+	}
+	p := Mine[len(Mine)-1]
+
+	return Send{p, bot.Victim, p.Ships}
+}
+
+func main() {
+	boilerplate.Run(&Mobber{})
+}

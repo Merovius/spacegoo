8eeb4863c89308d83c85dd973c250307946f1d9f
Axel Wagner <mail@merovius.de>
2013-06-21 02:42:39 +0200

    Expandbot: Better Endgame and Intercept-handling

--- expandbot/main.go
+++ expandbot/main.go
@@ -6,6 +6,29 @@ import (
 )
 
 type ExpandBot struct{}
+type FriedzClone struct{}
+
+var friedzclone FriedzClone
+
+func (bot *FriedzClone) Move(state GameState) Move {
+	mine := state.MyPlanets()
+	if len(mine) == 0 {
+		return Nop{}
+	}
+
+	mine = mine.SortByShips()
+	me := mine[len(mine)-1]
+
+	theirs := state.NotMyPlanets()
+	if len(theirs) == 0 {
+		return Nop{}
+	}
+
+	theirs = theirs.SortByDist(me.X, me.Y)
+	they := theirs[0]
+
+	return Send{me, they, me.Ships}
+}
 
 func isTargetedBy(state GameState, my Planet, his Planet) bool {
 	for _, fleet := range state.Fleets {
@@ -16,7 +39,35 @@ func isTargetedBy(state GameState, my Planet, his Planet) bool {
 	return false
 }
 
+func (bot *ExpandBot) EndMode(state GameState) Move {
+	mine := state.MyPlanets()
+	if len(mine) == 0 {
+		return Nop{}
+	}
+	for _, f := range state.Fleets {
+		if f.Owner != They {
+			continue
+		}
+
+		mine = mine.SortByDist(f.Target.X, f.Target.Y)
+		for _, mp := range mine {
+			if isTargetedBy(state, mp, f.Target) {
+				continue
+			}
+			return Send{mp, f.Target, mp.Ships}
+		}
+	}
+	return Nop{}
+}
+
 func (bot *ExpandBot) Move(state GameState) Move {
+	if len(state.TheirPlanets()) == 0 {
+		return bot.EndMode(state)
+	}
+	if state.PlayerName(They) == "intercept" {
+		return friedzclone.Move(state)
+	}
+
 	for _, mp := range state.MyPlanets() {
 		my := mp.Ships
 		for _, tp := range state.NotMyPlanets() {

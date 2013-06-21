d8bc517637890a7d808ca75ff21483df64cd0afb
Axel Wagner <mail@merovius.de>
2013-06-21 02:44:09 +0200

    Add Greedo-Bot
    Always trying to conquer the planet with the biggest production

---
+++ greedo/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	. "github.com/Merovius/spacegoo"
+	"github.com/Merovius/spacegoo/boilerplate"
+)
+
+type Greedo struct{}
+
+func (bot *Greedo) Move(state GameState) Move {
+	mine := state.MyPlanets()
+	if len(mine) == 0 {
+		return Nop{}
+	}
+
+	notmine := state.TheirPlanets()
+
+	var max Ships
+	var best Planet
+	var orig Planet
+	for _, p := range notmine {
+		if p.Production.Sum() <= max.Sum() {
+			continue
+		}
+
+		for _, mp := range mine {
+			nmy, _ := Simulate(mp.Ships, p.Ships)
+			if nmy.Sum() == 0 {
+				continue
+			}
+			max = p.Production
+			best = p
+			orig = mp
+		}
+	}
+
+	if max.Sum() > 0 {
+		return Send{orig, best, orig.Ships}
+	}
+
+	notmine = state.NeutralPlanets()
+	for _, p := range notmine {
+		if p.Production.Sum() <= max.Sum() {
+			continue
+		}
+
+		for _, mp := range mine {
+			nmy, _ := Simulate(mp.Ships, p.Ships)
+			if nmy.Sum() == 0 {
+				continue
+			}
+			max = p.Production
+			best = p
+			orig = mp
+		}
+	}
+
+	if max.Sum() > 0 {
+		return Send{orig, best, orig.Ships}
+	}
+	return Nop{}
+}
+
+func main() {
+	boilerplate.Run(&Greedo{})
+}

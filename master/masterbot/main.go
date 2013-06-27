ac524e9dd9a79bc5ea2c6c2f0e33e5a7eb41b74d
Axel Wagner <mail@merovius.de>
2013-06-27 23:27:57 +0200

    Actually add the masterbot

---
+++ master/masterbot/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"github.com/Merovius/spacegoo/master"
+	"log"
+	"strings"
+
+	// List of bots
+	_ "github.com/Merovius/spacegoo/clowder"
+	_ "github.com/Merovius/spacegoo/expandbot"
+	_ "github.com/Merovius/spacegoo/fofte"
+	_ "github.com/Merovius/spacegoo/friedzclone"
+	_ "github.com/Merovius/spacegoo/greedo"
+	_ "github.com/Merovius/spacegoo/haubitze"
+	_ "github.com/Merovius/spacegoo/hopper"
+	_ "github.com/Merovius/spacegoo/mnemo"
+	_ "github.com/Merovius/spacegoo/mobber"
+	_ "github.com/Merovius/spacegoo/nopbot"
+)
+
+var (
+	server = flag.String("server", "spacegoo.gpn.entropia.de:6000", "server address (host:port)")
+	user   string
+	pass   string
+)
+
+func main() {
+	flag.Parse()
+	name := flag.Arg(0)
+	pass := flag.Arg(1)
+	if name == "" || pass == "" {
+		log.Fatal("Expected botname password")
+	}
+	master.Run(name, *server, strings.ToLower(name), strings.ToLower(pass))
+}
